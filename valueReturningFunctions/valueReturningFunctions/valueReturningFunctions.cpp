411a4502d5ab34d4bcd070d31f57b67cbae65f9f
mbkamin <88458222+mbkamin@users.noreply.github.com>
2025-10-17 00:00:30 -0700

    added new functions

--- valueReturningFunctions/valueReturningFunctions/valueReturningFunctions.cpp
+++ valueReturningFunctions/valueReturningFunctions/valueReturningFunctions.cpp
@@ -10,11 +10,32 @@ int getIntFromUser()
 	return a;
 
 }
+void print()
+{
+	std::cout << "This is a void function that does not return a value.\n";
+}
+
+int doubleInt(int a)
+{
+	return 2 * a;
+}
+
+int tripleInt(int a)
+{
+	return 3 * a;
+}
 
 int main()
 {
 	int a{ getIntFromUser() };
     std::cout << "You entered " << a << "\n";
+
+	//using functions that take and return values
+	std::cout << "Double of " << a << " is " << doubleInt(a) << "\n";
+	std::cout << "Triple of " << a << " is " << tripleInt(a) << "\n";
+
+	std::cout << "Calling the void function:\n";
+	print();
 
 	return 0;
 }
